e8dd3d50736a035386b4d98ad36f3b172259f4b3
YanMer1 <mer21002@byui.edu>
2022-10-09 03:54:48 +0100

    Commit

--- chamber/scripts/last-modified.js
+++ chamber/scripts/last-modified.js
@@ -6,3 +6,23 @@ document.getElementById("lastModified").innerHTML = dateTime;
 let date = new Date();
 let year = date.getFullYear();
 document.getElementById("curYr").innerHTML = year;
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+	var x = document.getElementById("myLinks");
+	if (x.style.display === "block") {
+	  x.style.display = "none";
+	} else {
+	  x.style.display = "block";
+	}
+  }
