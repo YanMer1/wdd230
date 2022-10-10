f3a1f4f67f8fca03a2424c113153b0b6e3d0fbfb
YanMer1 <mer21002@byui.edu>
2022-10-10 22:43:39 +0100

    Commit

--- chamber/scripts/last-modified.js
+++ chamber/scripts/last-modified.js
@@ -16,3 +16,13 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 );
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+	  x.className += " responsive";
+	} else {
+	  x.className = "topnav";
+	}
+  }
