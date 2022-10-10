22aa3ea01b003d25472115c47895430db9f1a690
YanMer1 <mer21002@byui.edu>
2022-10-10 20:04:51 +0100

    Commit

--- chamber/scripts/last-modified.js
+++ chamber/scripts/last-modified.js
@@ -16,13 +16,3 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 );
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
-
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-	var x = document.getElementById("myLinks");
-	if (x.style.display === "block") {
-	  x.style.display = "none";
-	} else {
-	  x.style.display = "block";
-	}
-  }
