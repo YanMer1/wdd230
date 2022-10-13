6bede191863ee91d5f2829f6ae1b19fa5baef551
YanMer1 <mer21002@byui.edu>
2022-10-13 21:46:10 +0100

    Commit

--- lesson5/scripts/modification-script.js
+++ lesson5/scripts/modification-script.js
@@ -28,3 +28,12 @@ listBtn.addEventListener("click", function () {
 input.focus();
 }
 )
+
+let lastModified = new Date(document.lastModified);
+let fullDate = lastModified.toLocaleString('en-US', {month: "2-digit", day: "2-digit", year: "numeric"});
+let time = lastModified.toLocaleString('en-GB', {hour: "2-digit", minute: "2-digit", second: "2-digit"});
+let dateTime = `Last Updated: ${fullDate} ${time}`;
+document.getElementById("lastModified").innerHTML = dateTime;
+let date = new Date();
+let year = date.getFullYear();
+document.getElementById("curYr").innerHTML = year;
