d171430b01ebf032ff0ade969d0a9d0609d41f67
YanMer1 <mer21002@byui.edu>
2022-11-08 15:37:09 +0000

    Commit

--- chamber/scripts/last-modified.js
+++ chamber/scripts/last-modified.js
@@ -14,13 +14,3 @@ const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	now
 );
-
-datefield.innerHTML = `<em>${fulldate}</em>`;
-
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation')
-
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-
-// To solve the mid resizing issue with responsive class on
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
