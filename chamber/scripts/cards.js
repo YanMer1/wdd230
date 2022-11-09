94f774cd132106b9c850be17759477720ddd526f
YanMer1 <mer21002@byui.edu>
2022-11-09 00:31:35 +0000

    Commit

--- chamber/scripts/cards.js
+++ chamber/scripts/cards.js
@@ -1,4 +1,4 @@
-const URL = "https://antonio-saucedo.github.io/wdd230/chamber/json/data.json";
+const URL = "https://yanmer1.github.io/wdd230/chamber/json/data.json";
 
 const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
